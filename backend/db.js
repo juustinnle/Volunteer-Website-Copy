3caf402dab300ad0f427c4bd21b3c41a76accb73
juustinnle <158529422+juustinnle@users.noreply.github.com>
2024-07-25 00:30:08 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config(); // Add this line if you decide to use environment variables
+require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
